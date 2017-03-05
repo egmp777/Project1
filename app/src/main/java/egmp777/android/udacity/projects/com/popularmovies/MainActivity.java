3b4701761e87a386de96fe9e9304a7d7b0c496c5
egmp777 <egmp777@gmail.com>
2017-03-05 12:58:36 -0500

    Initial Code

---
+++ app/src/main/java/egmp777/android/udacity/projects/com/popularmovies/MainActivity.java
@@ -0,0 +1,13 @@
+package egmp777.android.udacity.projects.com.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
